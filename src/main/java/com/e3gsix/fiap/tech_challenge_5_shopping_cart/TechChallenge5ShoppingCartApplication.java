9412771cc93c195a649a77aa966be91da2adadae
Barreto <v.barreto.and@gmail.com>
2024-09-08 08:23:14 -0300

    initial commit

---
+++ src/main/java/com/e3gsix/fiap/tech_challenge_5_shopping_cart/TechChallenge5ShoppingCartApplication.java
@@ -0,0 +1,13 @@
+package com.e3gsix.fiap.tech_challenge_5_shopping_cart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechChallenge5ShoppingCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechChallenge5ShoppingCartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/e3gsix/fiap/tech_challenge_5_shopping_cart/TechChallenge5ShoppingCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.e3gsix.fiap.tech_challenge_5_shopping_cart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechChallenge5ShoppingCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
