aecbb8f796983909972d7fb80154988a86faab75
Barreto <v.barreto.and@gmail.com>
2024-09-10 19:13:42 -0300

    add whole api documentation with swagger

--- src/main/java/com/e3gsix/fiap/tech_challenge_5_shopping_cart/controller/impl/ShoppingCartControllerImpl.java
+++ src/main/java/com/e3gsix/fiap/tech_challenge_5_shopping_cart/controller/impl/ShoppingCartControllerImpl.java
@@ -5,6 +5,10 @@ import com.e3gsix.fiap.tech_challenge_5_shopping_cart.model.dto.request.Shopping
 import com.e3gsix.fiap.tech_challenge_5_shopping_cart.model.dto.response.PaymentIntegrityResponse;
 import com.e3gsix.fiap.tech_challenge_5_shopping_cart.model.dto.response.ShoppingCartResponse;
 import com.e3gsix.fiap.tech_challenge_5_shopping_cart.service.ShoppingCartService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.headers.Header;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +22,7 @@ import static com.e3gsix.fiap.tech_challenge_5_shopping_cart.controller.impl.Sho
 
 @RestController
 @RequestMapping(URL_SHOPPING_CART)
+@Tag(name = "Carrinho de Compras", description = "Gerenciamento de items em carrinhos de compra.")
 public class ShoppingCartControllerImpl implements ShoppingCartController {
 
     public static final String URL_SHOPPING_CART = "/shopping-carts";
@@ -31,6 +36,27 @@ public class ShoppingCartControllerImpl implements ShoppingCartController {
         this.shoppingCartService = shoppingCartService;
     }
 
+    @Operation(
+            summary = "Inclusão de item no Carrinho",
+            description = "Endpoint que possibilita a inclusão de itens em um Carrinho de Compras. O usuário terá " +
+                    "apenas um Carrinho de Compras ativo persistido, logo pode ser que o item apenas incremente a " +
+                    "quantidade de um determinado item no carrinho, assim como também pode ser que um novo carrinho " +
+                    "tenha de ser criado.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "201",
+                            description = "Item incluso no carrinho com sucesso.",
+                            headers = {
+                                    @Header(
+                                            name = "Location",
+                                            description = "Localização do carrinho ativo (onde o item foi adicionado)."
+                                    )
+                            }),
+                    @ApiResponse(responseCode = "400", description = "Inconsistência nos dados recebidos."),
+                    @ApiResponse(responseCode = "401", description = "Token não foi informado ou está inválido."),
+                    @ApiResponse(responseCode = "403", description = "Requisitante não tem permissões."),
+            }
+    )
     @Override
     @PostMapping(URL_SHOPPING_CART_ITEM)
     public ResponseEntity add(
@@ -48,6 +74,24 @@ public class ShoppingCartControllerImpl implements ShoppingCartController {
         return ResponseEntity.created(uri).build();
     }
 
+    @Operation(
+            summary = "Remoção de item no Carrinho",
+            description = "Recurso que retira itens do carrinho ativo identificando-o pelo ID.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "204",
+                            description = "Item removido do carrinho com sucesso.",
+                            headers = {
+                                    @Header(
+                                            name = "Location",
+                                            description = "Localização do carrinho ativo (onde o item foi removido)."
+                                    )
+                            }),
+                    @ApiResponse(responseCode = "400", description = "Inconsistência nos dados recebidos."),
+                    @ApiResponse(responseCode = "401", description = "Token não foi informado ou está inválido."),
+                    @ApiResponse(responseCode = "403", description = "Requisitante não tem permissões."),
+            }
+    )
     @Override
     @DeleteMapping(URL_SHOPPING_CART_ITEM)
     public ResponseEntity remove(
@@ -68,6 +112,17 @@ public class ShoppingCartControllerImpl implements ShoppingCartController {
         return new ResponseEntity(headers, HttpStatus.NO_CONTENT);
     }
 
+    @Operation(
+            summary = "Buscar Carrinho pelo ID",
+            description = "Funcionalidade de consultar um Carrinho de Compras",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Carrinho de Compras encontrado com sucesso."),
+                    @ApiResponse(responseCode = "400", description = "Inconsistência nos dados recebidos."),
+                    @ApiResponse(responseCode = "401", description = "Token não foi informado ou está inválido."),
+                    @ApiResponse(responseCode = "403", description = "Requisitante não tem permissões."),
+                    @ApiResponse(responseCode = "404", description = "Carrinho de Compras com ID informado não foi encontrado.")
+            }
+    )
     @Override
     @GetMapping(URL_SHOPPING_CART_BY_ID)
     public ResponseEntity<ShoppingCartResponse> findById(
@@ -80,6 +135,18 @@ public class ShoppingCartControllerImpl implements ShoppingCartController {
         return ResponseEntity.ok(response);
     }
 
+    @Operation(
+            summary = "Consultar integridade do Carrinho pelo ID",
+            description = "Endpoint que retorna a integridade do Carrinho de Compras, verificação importante para " +
+                    "conclusão, pois garante que se encontra em um estado válido para o pagamento.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Carrinho de Compras encontrado com sucesso."),
+                    @ApiResponse(responseCode = "400", description = "Inconsistência nos dados recebidos."),
+                    @ApiResponse(responseCode = "401", description = "Token não foi informado ou está inválido."),
+                    @ApiResponse(responseCode = "403", description = "Requisitante não tem permissões."),
+                    @ApiResponse(responseCode = "404", description = "Carrinho de Compras com ID informado não foi encontrado.")
+            }
+    )
     @Override
     @GetMapping(URL_SHOPPING_CART_BY_ID_PAYMENT_INTEGRITY)
     public ResponseEntity<PaymentIntegrityResponse> checkIntegrityForPayment(
@@ -92,6 +159,18 @@ public class ShoppingCartControllerImpl implements ShoppingCartController {
         return ResponseEntity.ok(response);
     }
 
+    @Operation(
+            summary = "Finalizar Carrinho de Compras",
+            description = "Funcionalidade que atualza o estado do carrinho para o estado final de concluído, " +
+                    "impossibilitando inclusão ou retirada de itens.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Carrinho de Compras encontrado com sucesso."),
+                    @ApiResponse(responseCode = "400", description = "Inconsistência nos dados recebidos."),
+                    @ApiResponse(responseCode = "401", description = "Token não foi informado ou está inválido."),
+                    @ApiResponse(responseCode = "403", description = "Requisitante não tem permissões."),
+                    @ApiResponse(responseCode = "404", description = "Carrinho de Compras com ID informado não foi encontrado.")
+            }
+    )
     @Override
     @PostMapping(URL_SHOPPING_CART_BY_ID)
     public ResponseEntity conclude(
--- src/main/java/com/e3gsix/fiap/tech_challenge_5_shopping_cart/security/SecurityConfig.java
+++ src/main/java/com/e3gsix/fiap/tech_challenge_5_shopping_cart/security/SecurityConfig.java
@@ -1,14 +1,18 @@
 package com.e3gsix.fiap.tech_challenge_5_shopping_cart.security;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.stereotype.Component;
+
+import static com.e3gsix.fiap.tech_challenge_5_shopping_cart.swagger.SwaggerConfig.*;
 
 @Component
 @EnableWebSecurity
@@ -26,10 +30,18 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.GET, URL_SWAGGER).permitAll()
+                        .requestMatchers(HttpMethod.GET, URL_SWAGGER_DEFAULT).permitAll()
+                        .requestMatchers(HttpMethod.GET, URL_SWAGGER_API).permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers("/swagger-ui/**", "/v3/api-docs/**");
     }
 
     @Bean
